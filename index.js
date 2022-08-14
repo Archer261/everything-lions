ae0625910f332aa61c3608287b33f3a68eb533a5
Joseph Archer <viewtiful.kja@gmail.com>
2022-08-14 17:23:14 -0400

    Port changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Instantiate package requirements
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
