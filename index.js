ce40e601ed277bcbe62fb977dd3a3a640852f585
Joseph Archer <viewtiful.kja@gmail.com>
2022-08-14 15:57:15 -0400

    Routes created for searching

--- index.js
+++ index.js
@@ -7,7 +7,63 @@ const axios = require('axios');
 // Call express function for usage
 const app = express();
 
-const espn = []
+const sources = [
+
+    {
+        name: "detroitlions.com",
+        address: "https://www.detroitlions.com/news",
+        filter: '',
+        link: ''
+    },
+    {
+        name: "detroitfreepress",
+        address: "https://www.freep.com/sports/lions",
+        filter: 'a[href*=story]',
+        link: 'https://www.freep.com/sports/lions'
+    },
+    {
+        name: "detroitfreepress",
+        address: "https://www.freep.com/sports/lions",
+        filter: 'a[href*=story]',
+        link: ''
+    },
+    {
+        name: "espn",
+        address: "https://www.espn.com/nfl/team/_/name/det/detroit-lions",
+        filter: 'a[href*=story]',
+        link: "https://www.espn.com"
+    },
+    {
+        name: "foxsports",
+        address: "https://www.foxsports.com/nfl/detroit-lions-team",
+        filter: 'a[href*=article]',
+        link: ""
+    }
+];
+const articles = []
+
+sources.forEach(s => {
+
+    axios.get(s.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a[href*=article],[href*=story]', html).each(function () {
+
+                const title = $(this).text();
+                const url = $(this).attr('href');
+
+                articles.push({
+                    title,
+                    url: s.link + url,
+                    source: s.name
+                })
+
+            })
+        })
+
+})
 
 app.get('/', function (req, res) {
     res.send("All about the Lions")
@@ -15,23 +71,36 @@ app.get('/', function (req, res) {
 })
 
 
-app.get('/bio', (req, res) => {
-    axios.get('https://www.espn.com/nfl/team/_/name/det/detroit-lions')
-        .then((response) => {
-            const html = response.data;
+app.get('/news', (req, res) => {
+    res.json(articles);
+})
+
+app.get('/news/:sourceId', async (req, res) => {
+    const sourceId = req.params.sourceId
+
+    const sorAddress = sources.filter(sor => sor.name == sourceId)[0].address
+    const sorlink = sources.filter(sor => sor.name == sourceId)[0].link
+    const sorFilter = sources.filter(sor => sor.name == sourceId)[0].filter
+
+    axios.get(sorAddress)
+        .then(response => {
+            const html = response.data
             const $ = cheerio.load(html)
 
-            $(`article > div > a[class="AnchorLink"]`, html).each(function () {
-                const title = $(this).attr('aria-label');
+            const specificSources = []
+
+            $(`${sorFilter}`, html).each(function () {
+                const title = $(this).text();
                 const url = $(this).attr('href');
-                characters.push({
+
+                specificSources.push({
                     title,
-                    url
+                    url: sorlink + url,
+                    source: sourceId
                 })
             })
-            res.json(characters);
-        }).catch((err) => console.log(err))
-
+            res.json(specificSources)
+        }).catch(err => console.log(err))
 })
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
